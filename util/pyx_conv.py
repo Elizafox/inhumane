b84d0c2ea72e8f88a603a6cf38f2a2fbebba1205
Elizabeth Myers <elizabeth@interlinked.me>
2013-10-15 22:38:33 -0500

    pyx_conv: cleanups

--- util/pyx_conv.py
+++ util/pyx_conv.py
@@ -8,6 +8,7 @@ import psycopg2
 import bs4
 import re
 import os
+import sys
 import shutil
 import json
 
@@ -33,46 +34,48 @@ def dehtmlify(string):
 
     return whitespace.sub(' ', soup.get_text())
 
+if len(sys.argv) >= 2:
+    path = sys.argv[1]
+    if os.path.exists(path):
+        if len(os.listdir(path)) > 0:
+            print("Not overwriting directory!")
+            quit(1)
+    else:
+        os.mkdir(path)
+
+    os.chdir(path)
+else:
+    print("Current dir implied...")
+
 conn = psycopg2.connect("dbname=pyx user=elizabeth")
 cur = conn.cursor()
 
-cur.execute("select * from black_cards")
-black_all = cur.fetchall()
-
-cur.execute("select * from white_cards")
-white_all = cur.fetchall()
-
-cur.execute("select * from card_set")
+cur.execute("SELECT id, active, name, base_deck, description FROM card_set")
 card_sets = cur.fetchall()
 
-cur.execute("select * from card_set_black_card")
-black_to_set = cur.fetchall()
+cur.execute("""SELECT text, draw, pick, watermark, card_set_id FROM black_cards
+            INNER JOIN card_set_black_card ON black_cards.id =
+            card_set_black_card.black_card_id""")
+black_cards = cur.fetchall()
 
-cur.execute("select * from card_set_white_card")
-white_to_set = cur.fetchall()
+cur.execute("""SELECT text, watermark, card_set_id FROM white_cards INNER JOIN
+            card_set_white_card ON white_cards.id =
+            card_set_white_card.white_card_id""")
+white_cards = cur.fetchall()
 
 conn.close()
 
-# Create ID to card maps
-black_map = dict()
-white_map = dict()
-for x in black_all:
-    id, text, draw, pick, watermark = x
-    black_map[id] = (text, draw, pick, watermark)
-
-for x in white_all:
-    id, text, watermark = x
-    white_map[id] = (text, watermark)
-
-# ID to names/descs
-id_to_set = dict()
-
-# Create control info for the card sets and the dirs needed
+card_set = dict()
 orig_dir = os.getcwd()
-for x in card_sets:
-    id, active, name, base_deck, desc, weight = x
+# Load all the card sets
+for item in card_sets:
+    (id, active, name, base, desc) = item 
+
+    # Cruft elimination
     name = name.replace('/', '-')
     name = name.replace('[CUSTOM] ', '')
+
+    name = dehtmlify(name)
 
     if os.path.exists(name):
         shutil.rmtree(name)
@@ -85,22 +88,20 @@ for x in card_sets:
                 'license' : 'Creative Commons',
                 'desc' : desc,
                 'name' : name,
-                'official' : base_deck,
+                'official' : base,
                }
         json.dump(info, f, sort_keys=True, indent=4)
 
     os.chdir(orig_dir)
 
-    id_to_set[id] = (name, desc)
+    card_set[id] = (active, name, base, desc)
 
-# Black cards formatting
-for x in black_to_set:
-    set_id, card_id = x
+# Black cards
+for item in black_cards:
+    (text, draw, pick, watermark, set_id) = item
 
-    name, desc = id_to_set[set_id]
-    os.chdir(name)
-
-    text, draw, pick, watermark = black_map[card_id]
+    deck_active, deck_name, base, deck_desc = card_set[set_id]
+    os.chdir(deck_name)
 
     # De-HTML-ify the text
     text = dehtmlify(text)
@@ -110,14 +111,13 @@ for x in black_to_set:
 
     os.chdir(orig_dir)
 
-# White cards formatting
-for x in white_to_set:
-    set_id, card_id = x
+# White cards
+for item in white_cards:
+    (text, watermark, set_id) = item
 
-    name, desc = id_to_set[set_id]
-    os.chdir(name)
+    deck_active, deck_name, base, deck_desc = card_set[set_id]
+    os.chdir(deck_name)
 
-    text, watermark = white_map[card_id]
     text = dehtmlify(text)
 
     with open('white.txt', 'a') as f:
