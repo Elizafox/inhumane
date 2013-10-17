6bcb6973e1f681aaac223d55915ee98fb4cf8c9c
Elizabeth Myers <elizabeth@interlinked.me>
2013-10-16 21:05:01 -0500

    Overhaul the deck loaders.
    They've been significantly cleaned up and they now also use the
    pkg_resources package from setuptools.

--- tests/test_deckloader.py
+++ tests/test_deckloader.py
@@ -3,6 +3,7 @@
 
 from inhumane.card import Deck
 from inhumane import deckloader
+from inhumane.deckloader import DeckLoadError
 import unittest
 import os.path
 
@@ -10,11 +11,11 @@ import os.path
 class DeckLoaderTestCase(unittest.TestCase):
     def test_loading_a_deck(self):
         """ Load a deck we know exists """
-        print(os.path.join(os.path.dirname(__file__), 'TestPack'))
-        a_deck = deckloader.load_deck(os.path.join(os.path.dirname(__file__), 'TestPack'))
+        deckpath = os.path.join(os.path.dirname(__file__), 'TestPack')
+        a_deck = deckloader.load_deck(deckpath)
         self.assertIsInstance(a_deck, Deck)
 
     def test_loading_a_nonexistant_deck(self):
         """ Load a deck we know doesn't exist """
-        self.assertRaises(FileNotFoundError, deckloader.load_deck, 'This pack does not exist')
+        self.assertRaises(DeckLoadError, deckloader.load_deck, "shittydeck")
 
