d3928a9dc3ae0bab11173a20041db1a6baaf5216
Elizabeth Myers <elizabeth@interlinked.me>
2015-06-29 01:35:08 -0500

    Allow private data for players

--- inhumane/player.py
+++ inhumane/player.py
@@ -10,12 +10,14 @@ _pc_lock = RLock()
 class Player(object):
     """ A player object for use in the game. """
 
-    def __init__(self, name):
+    def __init__(self, name, data=None):
         """ Initalise a player
 
         args:
-            name: name of the player"""
+            name: name of the player
+            data: private data"""
         self.name = name
+        self.data = data
 
         # Unique ID
         with _pc_lock:
@@ -31,10 +33,12 @@ class Player(object):
         return self.uid == other.uid
 
     def __hash__(self):
-        return hash(self.uid)
+        return hash(self.uid) + 1
 
     def __str__(self):
         return "{p}".format(p=self.name)
 
     def __repr__(self):
-        return "Player({x}, cid={i})".format(x=self.name, i=self.uid)
+        return "Player({x}, cid={i}, data={d!r})".format(x=self.name,
+                                                         i=self.uid,
+                                                         d=self.data)
